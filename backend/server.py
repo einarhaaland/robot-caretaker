26c269d2a5789ddd8d73169bf62f71f3b84657bc
Einar <einarhaaland98@gmail.com>
2022-08-22 17:19:05 +0200

    add route for testing robot connection

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def test():
     return {"Test": "OK"}
 
+@app.route("/test_robot_connection")
+def test_robot_connection():
+    # Do call to robot api
+    # if success return status good
+    status_code = 200
+    return {"status": status_code}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
