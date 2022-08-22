ec5c1ad8eb747a528dac2db373e221abacd2564d
Einar <einarhaaland98@gmail.com>
2022-08-22 17:18:30 +0200

    add button for testing robot connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import FetchButton from './components/FetchButton';
 
 function App() {
 
   useEffect(() => {
+    console.log("TESTING BACKEND CONNECTION..")
     fetch("/test")
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
+        console.log("..DONE")
       })
   }, [])
 
   return (
-    <div className="App">
-      kult
-    </div>
+    <FetchButton />
   );
 }
 
---
+++ frontend/src/components/FetchButton.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+function FetchButton() {
+    const [result, setResult] = useState<{status: number}>()
+
+    const handleClick = () => {
+        console.log("TESTING ROBOT CONNECTION..")
+        fetch("/test_robot_connection")
+            .then((res) => res.json())
+            .then((data) => {
+                setResult(data)
+                console.log("..DONE")
+            })
+    }
+
+    return (
+        <>
+            <button onClick={handleClick}>TEST ROBOT CONNECTION</button>
+            <p>{result && "STATUS: " + result.status}</p>
+        </>
+    );
+}
+
+export default FetchButton;
