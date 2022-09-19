0078b70706e902c8494f6a4d2d5b7e7beefca0fe
Einar Haaland <einarhaaland98@gmail.com>
2022-09-19 15:18:20 +0200

    add .xar to .motion converter

---
+++ webots_implementation/motions/xar_to_motion.py
@@ -0,0 +1,48 @@
+'''
+Script to help convert .xar files found /created in Choregraphe to .MOTION files that can be used by Webots.
+pip install beautifulsoup4
+pip install lxlml
+
+To use:
+Paste path to xar file in global variable FILE_PATH
+Adjust N_POSTITIONS
+Run
+Needed output is printed to console
+
+
+Might be off by 1 index
+'''
+
+from bs4 import BeautifulSoup as bs
+
+FILE_PATH = "C:/Users/einar/Desktop/excited.xar"
+N_POSITIONS = 55
+
+with open(FILE_PATH, "r") as file:
+    content = file.readlines()
+    content = "".join(content)
+    bs_content = bs(content, "xml")
+
+    # All tags where a joint is moved
+    tags = bs_content.findAll("ActuatorCurve")
+
+    # Populate result
+    result = [ [] for i in range(N_POSITIONS) ]
+    for i in range(N_POSITIONS):
+        for j in range(len(tags)):
+            result[i].append('*')
+
+    for tag in tags:
+        # All keyframes of a joint
+        keyframes = list(tag.findAll("Key"))
+
+        # The frame-numbers animation is performed on
+        actionframes = []
+        for frame in keyframes:
+            actionframes.append(frame['frame'])
+        
+        for frame in keyframes:
+            result[int(frame['frame'])][tags.index(tag)] = frame['value']
+
+    print(result)
+        
