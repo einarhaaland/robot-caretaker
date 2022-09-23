e84be998915e66181cc6a90da0662fe4edccd3b2
Einar <einarhaaland98@gmail.com>
2022-09-23 15:10:18 +0200

    add exchange and more params + doc

--- backend/messaging/send.py
+++ backend/messaging/send.py
@@ -1,16 +1,36 @@
 import pika
 
-def send(body, host='localhost', queue='hello', routing_key='hello', exchange=''):
-    '''
-    Sends a message using given args
-    '''
+'''
+Publishes a message using Pika -> RabbitMQ
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+Queue is declared and bound to exhange in the Consumer,
+
+ARGS:
+    body:           The content to send                         (JSON)
+    host:           The host of the RabbitMQ broker instance    (Str)
+    routing_key:    Address where exhange routes message        (Str)
+    exchange:       Name of exchange                            (Str)
+    exchange_type:  "direct", "topic", "fanout", "headers"      (Str)
+'''
+
+def send(
+    body, 
+    host='localhost',  
+    routing_key='webots', 
+    exchange='routing_exchange', 
+    exchange_type='fanout'
+    ):
+
+    # Init connection and channel
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     channel = connection.channel()
 
-    channel.queue_declare(queue=queue)
+    # Declare exchange
+    channel.exchange_declare(exchange=exchange, exchange_type=exchange_type)
 
+    # Publish message
     channel.basic_publish(exchange=exchange, routing_key=routing_key, body=body)
     print(f"Sent '{body}'")
 
+    # Close connection
     connection.close()
