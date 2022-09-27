77919a51824ed2596861e278185cfa888fdc8437
Einar <einarhaaland98@gmail.com>
2022-09-27 15:44:34 +0200

    implement simple nao controller

--- webots_implementation/controllers/example/nao_emotion_controller.py
+++ webots_implementation/controllers/example/nao_emotion_controller.py
@@ -3,8 +3,8 @@ For this file to be found, folder needs to have same name as this file:
     nao_emotion_controller/nao_emotion_controller.py
 '''
 
-
 import threading
+from controller import Robot, Keyboard, Motion
 from super_controller import SuperController
 
 
@@ -13,8 +13,23 @@ class NaoEmotionController(SuperController):
     Nao-specific controller.
     Sub class of ../Controller.py
     '''
+
+    # Constructor
     def __init__(self):
         super().__init__()
+
+    # Load .Motion files
+    def loadMotionFiles(self):
+        self.handWave = Motion('../../motions/HandWave.motion')
+        self.nod = Motion('../../motions/Nod.motion')
+        self.cheer = Motion('../../motions/Cheer.motion')
+        self.shakeHead = Motion('../../motions/ShakeHead.motion')
+        self.excited = Motion('../../motions/Excited.motion')
+        self.thinking = Motion('../../motions/Thinking.motion')
+
+    def messageCallback(channel, method, properties, body):
+        motioncall = "self." + body.decode("utf-8")
+        startMotion(eval(motioncall))
 
 
 def main():
