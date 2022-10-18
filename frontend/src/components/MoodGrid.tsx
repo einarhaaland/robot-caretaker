2bdf178187f93514c88be403d4170a26887e7324
Einar Haaland <einarhaaland98@gmail.com>
2022-10-18 14:02:49 +0200

    make grid a react state

--- frontend/src/components/MoodGrid.tsx
+++ frontend/src/components/MoodGrid.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Grid } from '@mui/material';
 import MoodCard from './MoodCard';
 
 
 function MoodGrid() {
-    return (
-        <Grid container spacing={3}>
-          <Grid item>
-            <MoodCard title='Cheer'/>
-          </Grid>
-          <Grid item>
-            <MoodCard title='Nod'/>
-          </Grid>
-          <Grid item>
-            <MoodCard title='Wave'/>
-          </Grid>
-          <Grid item>
-            <MoodCard title='ShakeHead'/>
-          </Grid>
-          <Grid item>
-            <MoodCard title='Thinking'/>
-          </Grid>
+  const [cards, setCards] = useState<React.ReactElement[]>([]);
+
+  useEffect(() => {
+    //Cards can be fetched from your database here to keep custom cards when server resets
+    setCards([
+        <Grid item>
+          <MoodCard title='Cheer'/>
+        </Grid>,
+        <Grid item >
+          <MoodCard title='Nod'/>
+        </Grid>,
+        <Grid item>
+          <MoodCard title='Wave'/>
+        </Grid>,
+        <Grid item>
+          <MoodCard title='ShakeHead'/>
+        </Grid>,
+        <Grid item>
+          <MoodCard title='Thinking'/>
         </Grid>
-    );
+    ])
+  }, []);
+
+
+  return (
+    <Grid container spacing={3}>
+      {cards}
+    </Grid>
+  );
 }
 
 export default MoodGrid;
