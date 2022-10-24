8a90ad4c0e936028a6141e43c08def6c7778edea
Einar Haaland <einarhaaland98@gmail.com>
2022-10-24 13:12:08 +0200

    make navbar buttons open link in new tab

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,9 +12,9 @@ const Navbar = () => {
                     Robot Emotion Controller
                 </Typography>
                 <Stack direction="row" spacing={2}>
-                    <Button color="inherit" href="https://github.com/einarhaaland/robot-caretaker">About</Button>
-                    <Button color="inherit" href="https://github.com/einarhaaland/robot-caretaker#how-to-use">How To Use</Button>
-                    <Button color="inherit" href="https://github.com/einarhaaland/robot-caretaker#adding-a-new-robot">Add Robot</Button>
+                    <Button color="inherit" target="_blank" href="https://github.com/einarhaaland/robot-caretaker">About</Button>
+                    <Button color="inherit" target="_blank" href="https://github.com/einarhaaland/robot-caretaker#how-to-use">How To Use</Button>
+                    <Button color="inherit" target="_blank" href="https://github.com/einarhaaland/robot-caretaker#adding-a-new-robot">Add Robot</Button>
                 </Stack>
             </Toolbar>
         </AppBar>
