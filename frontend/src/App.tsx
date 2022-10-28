433ccdc92f112eeaee8b2f6012723d59b469f88f
Einar Haaland <einarhaaland98@gmail.com>
2022-10-28 12:28:12 +0200

    move form to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,80 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import NavBar from './components/Navbar';
 import MoodGrid from './components/MoodGrid';
+import { IconButton } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
+  const [posCards, setPosCards] = useState(["Cheer", "Nod"])
+  const [neuCards, setNeuCards] = useState(["Wave", "Thinking"])
+  const [negCards, setNegCards] = useState(["ShakeHead"])
+  const [showFormAdd, setShowFormAdd] = useState(false)
+  const [showFormDelete, setShowFormDelete] = useState(false)
 
-  useEffect(() => {
-    console.log("TESTING BACKEND CONNECTION..")
-    fetch("http://localhost:5000/test")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-        console.log("..DONE")
-      })
-  }, [])
+  const handleSubmitAdd = (e: any) => {
+    e.preventDefault();
+    setShowFormAdd(false)
+  }
+
+  const handleSubmitDelete = (e: any) => {
+    e.preventDefault();
+    for (let i = 0; i < posCards.length; i++) {
+      if (posCards[i] == e.target.mood.value) {
+        if (e.target.sentiment.value == "Positive") {
+          setPosCards( (arr) => {
+            arr.splice(i, 1);
+            return arr;
+          });
+        }
+        else if (e.target.sentiment.value == "Neutral") {
+          setNeuCards( (arr) => {
+            arr.splice(i, 1);
+            return arr;
+          });
+        }
+        else if (e.target.sentiment.value == "Positive") {
+          setNegCards( (arr) => {
+            arr.splice(i, 1);
+            return arr;
+          });
+        }
+        break;
+      }
+    }
+    setShowFormDelete(false)
+  }
 
   return (
     <>
       <NavBar />
-      <MoodGrid moodCards={["Cheer", "Nod"]} color="#e7e6f7" />
-      <MoodGrid moodCards={["Wave", "Thinking"]} color="#e3d0d8" />
-      <MoodGrid moodCards={["ShakeHead"]} color="#aea3b0" />
+      <MoodGrid moodCards={posCards} color="#e7e6f7" />
+      <MoodGrid moodCards={neuCards} color="#e3d0d8" />
+      <MoodGrid moodCards={negCards} color="#aea3b0" />
+
+      {
+        showFormAdd && 
+        <form onSubmit={handleSubmitAdd} style={{position:'absolute', left:'30%', bottom:'50%'}}>
+            Enter name of new mood-function:
+            <input type={"text"} name={"mood"}/>
+        </form> 
+      }
+      {
+        showFormDelete && 
+        <form onSubmit={handleSubmitDelete} style={{position:'absolute', left:'30%', bottom:'50%'}}>
+            Enter name of mood-function to delete:
+            <input type={"text"} name={"mood"}/>
+        </form> 
+      }
+
+      <IconButton aria-label="add" size="large" onClick={() => setShowFormAdd(true)} sx={{position:'absolute', bottom:0, right:100, fontSize:"5rem", color:'#4CAF50'}}>
+        <AddCircleIcon fontSize='inherit'/>
+      </IconButton>
+
+      <IconButton aria-label="add" size="large" onClick={() => setShowFormDelete(true)} sx={{position:'absolute', bottom:0, right:0, fontSize:"5rem", color:'#e41b36'}}>
+        <DeleteIcon fontSize='inherit'/>
+      </IconButton>
     </>
   );
 }
--- frontend/src/components/MoodGrid.tsx
+++ frontend/src/components/MoodGrid.tsx
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import MoodCard from './MoodCard';
-import { Grid, IconButton } from '@mui/material';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Grid } from '@mui/material';
 
 
 interface GridProps {
@@ -13,68 +11,19 @@ interface GridProps {
 
 function MoodGrid(props: GridProps) {
   const [cards, setCards] = useState<React.ReactElement[]>([]);
-  const [showFormAdd, setShowFormAdd] = useState(false)
-  const [showFormDelete, setShowFormDelete] = useState(false)
-
 
   useEffect(() => {
-    //Cards can be fetched from your database here to keep custom cards when server resets
+    //setCards([]);
     for (let i = 0; i < props.moodCards.length; i++) {
       setCards((oldArray) => [...oldArray, <Grid item><MoodCard title={props.moodCards[i]}/></Grid>])
     }
   }, []);
 
-  const handleSubmitAdd = (e: any) => {
-    e.preventDefault();
-    setCards((old) => [...old, <Grid item><MoodCard title={e.target.mood.value}/></Grid>])
-    setShowFormAdd(false)
-  }
-
-  const handleSubmitDelete = (e: any) => {
-    e.preventDefault();
-    for (let i = 0; i < cards.length; i++) {
-      if (cards[i].props.children.props.title == e.target.mood.value) {
-        setCards( (arr) => {
-          arr.splice(i, 1);
-          return arr;
-        });
-        break;
-      }
-    }
-    setShowFormDelete(false)
-  }
-
-
   return (
     <>
-
       <Grid container xs={4} spacing={3} style={{backgroundColor: props.color, margin: "0px", display: "inline-flex", height: "100vh"}}>
         {cards}
       </Grid>
-
-      {
-        showFormAdd && 
-        <form onSubmit={handleSubmitAdd} style={{position:'absolute', left:'30%', bottom:'50%'}}>
-            Enter name of new mood-function:
-            <input type={"text"} name={"mood"}/>
-        </form> 
-      }
-      {
-        showFormDelete && 
-        <form onSubmit={handleSubmitDelete} style={{position:'absolute', left:'30%', bottom:'50%'}}>
-            Enter name of mood-function to delete:
-            <input type={"text"} name={"mood"}/>
-        </form> 
-      }
-
-      <IconButton aria-label="add" size="large" onClick={() => setShowFormAdd(true)} sx={{position:'absolute', bottom:0, right:100, fontSize:"5rem", color:'#4CAF50'}}>
-        <AddCircleIcon fontSize='inherit'/>
-      </IconButton>
-
-      <IconButton aria-label="add" size="large" onClick={() => setShowFormDelete(true)} sx={{position:'absolute', bottom:0, right:0, fontSize:"5rem", color:'#e41b36'}}>
-        <DeleteIcon fontSize='inherit'/>
-      </IconButton>
-
     </>
   );
 }
